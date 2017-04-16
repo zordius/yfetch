70000e88e7c470aa654876f0138634b10c27a5fc
Zordius Chen <zordius@yahoo-inc.com>
2017-04-16 13:58:34 +0800

    eslint fix done

--- src.js
+++ src.js
@@ -1,67 +1,78 @@
-import { stringify } from 'querystring'
-import debug from 'debug'
-const debugRaw = debug('yfetch:raw')
-const debugResult = debug('yfetch:result')
-const debugError = debug('yfetch:error')
- 
+import { stringify } from 'querystring';
+import debug from 'debug';
+
+const debugRaw = debug('yfetch:raw');
+const debugResult = debug('yfetch:result');
+const debugError = debug('yfetch:error');
+
+export class YfetchError extends Error {
+}
+
 // Support simple opts.query , opts.base and opts.url logic
 // return fetch() arguments
-export const transformFetchOptions = ({query, base = '', url = '', ...opts})  => {
+export const transformFetchOptions = ({ query, base = '', url = '', ...opts }) => {
   const queryString = stringify(query);
-  const urlString = base + url + (queryString ? ('?' + queryString)  : '');
- 
+  const urlString = base + url + (queryString ? `?${queryString}` : '');
+
   return [urlString, opts];
-}
- 
+};
+
 // Support opts.json and opts.error , send debug yfetch:result
 export const transformFetchResult = (context = {}) => {
-  const {fetchArgs, ...response} = context;
- 
+  const { fetchArgs, ...response } = context;
+
   if (fetchArgs[1].json) {
     response.body = JSON.parse(response.body);
   }
- 
+
   debugResult('url: %s - status: %s - body: %O', response.url, response.status, response.body);
 
-  if (fetchArgs[1].error && fetchArgs[1].error.indexOf && ~fetchArgs[1].error.indexOf(response.status)) {
-    throw new yfetchError(`Response Code ${response.status} means Error (includes: ${fetchArgs[1].error.join(',')})`);
+  if (fetchArgs[1].error
+      && fetchArgs[1].error.indexOf
+      && fetchArgs[1].error.indexOf(response.status) > 0) {
+    throw new YfetchError(`Response Code ${response.status} means Error (includes: ${fetchArgs[1].error.join(',')})`);
   }
- 
-  return {...response, fetchArgs};
-}
- 
-// handle response.text() promise, make response simple, keep response.fetchArgs. send debug yfetch:raw
+
+  return { ...response, fetchArgs };
+};
+
+// handle response.text() promise, make response simple, keep response.fetchArgs.
+// send debug yfetch:raw
 const transformForContext = fetchArgs => (response = {}) => {
-  const {url, status, statusText, headers = [], ok, body, size} = response;
+  const { url, status, statusText, headers = [], ok, size } = response;
   const H = {};
- 
+
   headers.forEach((v, k) => {
     H[k] = v;
   });
- 
-  let job = response.text ? response.text() : Promise.resolve();
- 
+
+  const job = response.text ? response.text() : Promise.resolve();
+
   return job
-  .then((bodyText) => {
-    debugRaw('url: %s - status: %s - size: %s - header: %o - body: %s', url, status, size, H, bodyText);
- 
+  .then((body) => {
+    debugRaw('url: %s - status: %s - size: %s - header: %o - body: %s', url, status, size, H, body);
+
     return {
-      url, status, statusText, ok, size,
+      url,
+      status,
+      statusText,
+      ok,
+      size,
+      body,
+      fetchArgs,
       headers: H,
-      body: bodyText,
-      fetchArgs: fetchArgs
     };
   });
-}
- 
+};
+
 // keep error.fetchArgs and error.response , send debug yfetch:error
-const transformFetchError = (fetchArgs, response) => error => {
+const transformFetchError = (fetchArgs, response) => (error) => {
   debugError('url: %s - status: %s - size: %s - body: %s - %O', fetchArgs[0], response.status, response.size, response.body, error);
   error.fetchArgs = fetchArgs;
   error.response = response;
   throw error;
-}
- 
+};
+
 // Export this for unit testing and mock
 export const executeFetch = args => fetch(...args);
 
