51f40dd4e18af039b6de733091299c2915e4a464
Zordius Chen <zordius@yahoo-inc.com>
2017-04-16 13:36:34 +0800

    eslint fix

--- spec/mockHttp.js
+++ spec/mockHttp.js
@@ -3,9 +3,9 @@ import nock from 'nock';
 export const MOCK_HOST = 'http://test';
 
 const MOCK_CONFIG = {
-    OK: ['/ok', 'Yes, OK'],
-    OK_JSON: ['/ok/json', {yes: 'ok', payload: {is: 'good'}}],
-    OK_DELAY2: ['/ok/delay2', 'delay 2 seconds', 200, 2000]
+  OK: ['/ok', 'Yes, OK'],
+  OK_JSON: ['/ok/json', { yes: 'ok', payload: { is: 'good' } }],
+  OK_DELAY2: ['/ok/delay2', 'delay 2 seconds', 200, 2000],
 };
 
 const errorNotEnabled = 'Please run enableMockHttp first!';
@@ -13,24 +13,24 @@ export let MOCK_URLS = errorNotEnabled;
 export let MOCK_BODY = errorNotEnabled;
 
 export const enableMockHttp = () => {
-    MOCK_URLS = {};
-    MOCK_BODY = {};
+  MOCK_URLS = {};
+  MOCK_BODY = {};
 
-    let N = nock(MOCK_HOST).persist();
+  let N = nock(MOCK_HOST).persist();
 
-    Object.entries(MOCK_CONFIG).forEach(([name, [path, body, code = 200, delay = 0]]) => {
-        N = N.get(path);
-        if (delay) {
-            N = N.delayBody(delay);
-        }
-        N = N.reply(code, body);
-        MOCK_URLS[name] = MOCK_HOST + path;
-        MOCK_BODY[name] = body;
-    });
-}
+  Object.entries(MOCK_CONFIG).forEach(([name, [path, body, code = 200, delay = 0]]) => {
+    N = N.get(path);
+    if (delay) {
+      N = N.delayBody(delay);
+    }
+    N = N.reply(code, body);
+    MOCK_URLS[name] = MOCK_HOST + path;
+    MOCK_BODY[name] = body;
+  });
+};
 
 export const disableMockHttp = () => {
-    MOCK_URLS = errorNotEnabled;
-    MOCK_BODY = errorNotEnabled;
-    nock.cleanAll();
-}
+  MOCK_URLS = errorNotEnabled;
+  MOCK_BODY = errorNotEnabled;
+  nock.cleanAll();
+};
--- src.js
+++ src.js
@@ -7,76 +7,76 @@ const debugError = debug('yfetch:error')
 // Support simple opts.query , opts.base and opts.url logic
 // return fetch() arguments
 export const transformFetchOptions = ({query, base = '', url = '', ...opts})  => {
-    const queryString = stringify(query);
-    const urlString = base + url + (queryString ? ('?' + queryString)  : '');
+  const queryString = stringify(query);
+  const urlString = base + url + (queryString ? ('?' + queryString)  : '');
  
-    return [urlString, opts];
+  return [urlString, opts];
 }
  
 // Support opts.json and opts.error , send debug yfetch:result
 export const transformFetchResult = (context = {}) => {
-    const {fetchArgs, ...response} = context;
+  const {fetchArgs, ...response} = context;
  
-    if (fetchArgs[1].json) {
-        response.body = JSON.parse(response.body);
-    }
+  if (fetchArgs[1].json) {
+    response.body = JSON.parse(response.body);
+  }
  
-    debugResult('url: %s - status: %s - body: %O', response.url, response.status, response.body);
+  debugResult('url: %s - status: %s - body: %O', response.url, response.status, response.body);
 
-    if (fetchArgs[1].error && fetchArgs[1].error.indexOf && ~fetchArgs[1].error.indexOf(response.status)) {
-        throw new yfetchError(`Response Code ${response.status} means Error (includes: ${fetchArgs[1].error.join(',')})`);
-    }
+  if (fetchArgs[1].error && fetchArgs[1].error.indexOf && ~fetchArgs[1].error.indexOf(response.status)) {
+    throw new yfetchError(`Response Code ${response.status} means Error (includes: ${fetchArgs[1].error.join(',')})`);
+  }
  
-    return {...response, fetchArgs};
+  return {...response, fetchArgs};
 }
  
 // handle response.text() promise, make response simple, keep response.fetchArgs. send debug yfetch:raw
 const transformForContext = fetchArgs => (response = {}) => {
-    const {url, status, statusText, headers = [], ok, body, size} = response;
-    const H = {};
+  const {url, status, statusText, headers = [], ok, body, size} = response;
+  const H = {};
  
-    headers.forEach((v, k) => {
-        H[k] = v;
-    });
+  headers.forEach((v, k) => {
+    H[k] = v;
+  });
  
-    let job = response.text ? response.text() : Promise.resolve();
+  let job = response.text ? response.text() : Promise.resolve();
  
-    return job
-    .then((bodyText) => {
-        debugRaw('url: %s - status: %s - size: %s - header: %o - body: %s', url, status, size, H, bodyText);
+  return job
+  .then((bodyText) => {
+    debugRaw('url: %s - status: %s - size: %s - header: %o - body: %s', url, status, size, H, bodyText);
  
-        return {
-            url, status, statusText, ok, size,
-            headers: H,
-            body: bodyText,
-            fetchArgs: fetchArgs
-        };
-    });
+    return {
+      url, status, statusText, ok, size,
+      headers: H,
+      body: bodyText,
+      fetchArgs: fetchArgs
+    };
+  });
 }
  
 // keep error.fetchArgs and error.response , send debug yfetch:error
 const transformFetchError = (fetchArgs, response) => error => {
-    debugError('url: %s - status: %s - size: %s - body: %s - %O', fetchArgs[0], response.status, response.size, response.body, error);
-    error.fetchArgs = fetchArgs;
-    error.response = response;
-    throw error;
+  debugError('url: %s - status: %s - size: %s - body: %s - %O', fetchArgs[0], response.status, response.size, response.body, error);
+  error.fetchArgs = fetchArgs;
+  error.response = response;
+  throw error;
 }
  
 // Export this for unit testing and mock
-export const executeFetch = (args) => fetch(...args);
- 
+export const executeFetch = args => fetch(...args);
+
 // The main yfetch function
 export const yfetch = (opts = {}) => {
-    const fetchArgs = transformFetchOptions(opts);
-    let R = {};
- 
-    // module.exports.executeFetch allow jasmine to mock it
-    return module.exports.executeFetch(fetchArgs)
-    .then(transformForContext(fetchArgs))
-    .then((response) => Object.assign(R, response))
-    .then(transformFetchResult)
-    .catch(transformFetchError(fetchArgs, R));
-}
- 
+  const fetchArgs = transformFetchOptions(opts);
+  const R = {};
+
+  // module.exports.executeFetch allow jasmine to mock it
+  return module.exports.executeFetch(fetchArgs)
+  .then(transformForContext(fetchArgs))
+  .then(response => Object.assign(R, response))
+  .then(transformFetchResult)
+  .catch(transformFetchError(fetchArgs, R));
+};
+
 // as default
-export default yfetch
+export default yfetch;
