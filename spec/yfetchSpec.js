8f4329f0ad78a1ac8c954e585b421e2f59a6974b
Zordius Chen <zordius@yahoo-inc.com>
2017-05-27 20:12:29 +0800

    add test

--- spec/yfetchSpec.js
+++ spec/yfetchSpec.js
@@ -93,6 +93,10 @@ describe(`yfetch [${target}.js]`, () => {
       it('should ignore bad opts.error', () => yfetch({ url: MOCK_URLS.OK, error: 1 }));
       it('should ignore empty opts.error', () => yfetch({ url: MOCK_URLS.OK, error: [] }));
       it('should success when opts.error not matched', () => yfetch({ url: MOCK_URLS.OK, error: [404, 500] }));
+      it('should threat code 200 as error', () => yfetch({ url: MOCK_URLS.OK, error: [200] }).then(fail, (err) => {
+        expect(err.message).toEqual('Response Code 200 means Error (includes: 200)');
+        })
+      );
       it('should threat code 200 as error', done => yfetch({ url: MOCK_URLS.OK, error: [100, 200, 300] }).then(fail, (err) => {
         expect(err.message).toEqual('Response Code 200 means Error (includes: 100,200,300)');
         expect(err.response).toEqual({
