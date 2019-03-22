4f87f3f457dbeaee6f8cc1f96087a7fad6159ccd
Zordius Chen <1201409+zordius@users.noreply.github.com>
2019-03-22 11:07:50 +0800

    Refine tests

--- spec/yfetch.spec.js
+++ spec/yfetch.spec.js
@@ -1,5 +1,5 @@
 import 'isomorphic-fetch'
-import { enableMockHttp, disableMockHttp, MOCK_URLS, MOCK_BODY } from './mockHttp'
+import { enableMockHttp, disableMockHttp, MOCK_HOST, MOCK_URLS, MOCK_BODY } from './mockHttp'
 
 const target = process.env.TESTTARGET || 'src'
 const yFetch = require(`../${target}`)
@@ -60,64 +60,101 @@ describe(`yfetch [${target}.js]`, () => {
       beforeAll(enableMockHttp)
       afterAll(disableMockHttp)
 
-      it('should be ok', () => yfetch({ url: MOCK_URLS.OK }))
-
-      it('should failed', () => yfetch({ url: '_should_error_' }))
-
-      it('should failed with context', () => yfetch({ url: '_should_error_' }).catch((error) => {
-        expect(error.response).toEqual({})
-        expect(error.fetchArgs).toEqual(['_should_error_', { headers: {} }])
-      }))
-
-      it('should receive full context of fetch', () => yfetch({ url: MOCK_URLS.OK }).then((context) => {
-        expect(context).toEqual({
-          url: MOCK_URLS.OK,
-          headers: {},
-          status: 200,
-          statusText: 'OK',
-          ok: true,
-          body: MOCK_BODY.OK,
-          size: 0,
-          fetchArgs: [MOCK_URLS.OK, { headers: {} }]
+      it('should be ok', () => {
+        expect.assertions(1)
+        return yfetch({ url: MOCK_URLS.OK }).then(result => {
+          expect(result.body).toEqual('Yes, OK')
         })
-      }))
+      })
 
-      it('should support timeout', () => yfetch({ url: MOCK_URLS.OK_DELAY2, timeout: 100 }).catch((error) => {
-        expect(error.response).toEqual({})
-        expect(error.fetchArgs).toEqual([MOCK_URLS.OK_DELAY2, { timeout: 100, headers: {} }])
-      }))
+      it('should failed with context', () => {
+        expect.assertions(2)
+        return yfetch({ url: MOCK_HOST + '/_should_error_' }).catch((error) => {
+          expect(error.response).toEqual({})
+          expect(error.fetchArgs).toEqual(['http://test/_should_error_', { headers: {} }])
+        })
+      })
+
+      it('should receive full context of fetch', () => {
+        expect.assertions(1)
+        return yfetch({ url: MOCK_URLS.OK }).then((context) => {
+          return expect(context).toEqual({
+            url: MOCK_URLS.OK,
+            headers: {},
+            status: 200,
+            statusText: 'OK',
+            ok: true,
+            body: MOCK_BODY.OK,
+            size: 0,
+            fetchArgs: [MOCK_URLS.OK, { headers: {} }]
+          })
+        })
+      })
+
+      it('should support timeout', () => {
+        expect.assertions(2)
+        return yfetch({ url: MOCK_URLS.OK_DELAY2, timeout: 100 }).catch((error) => {
+          expect(error.response).toEqual({})
+          expect(error.fetchArgs).toEqual([MOCK_URLS.OK_DELAY2, { timeout: 100, headers: {} }])
+        })
+      })
 
       // json
-      it('should response text when no opts.json', () => yfetch({ url: MOCK_URLS.OK_JSON }).then((response) => {
-        expect(response.body).toEqual(JSON.stringify(MOCK_BODY.OK_JSON))
-      }))
-      it('should handle JSON when opts.json is true', () => yfetch({ url: MOCK_URLS.OK_JSON, json: true }).then((response) => {
-        expect(response.body).toEqual(MOCK_BODY.OK_JSON)
-      }))
+      it('should response text when no opts.json', () => {
+        expect.assertions(1)
+        return yfetch({ url: MOCK_URLS.OK_JSON }).then((response) => {
+          expect(response.body).toEqual(JSON.stringify(MOCK_BODY.OK_JSON))
+        })
+      })
+
+      it('should handle JSON when opts.json is true', () => {
+        expect.assertions(1)
+        return yfetch({ url: MOCK_URLS.OK_JSON, json: true }).then((response) => {
+          expect(response.body).toEqual(MOCK_BODY.OK_JSON)
+        })
+      })
 
       // error
-      it('should ignore bad opts.error', () => yfetch({ url: MOCK_URLS.OK, error: 1 }))
-      it('should ignore empty opts.error', () => yfetch({ url: MOCK_URLS.OK, error: [] }))
-      it('should success when opts.error not matched', () => yfetch({ url: MOCK_URLS.OK, error: [404, 500] }))
+      it('should ignore bad opts.error', () => {
+        expect.assertions(1)
+        return yfetch({ url: MOCK_URLS.OK, error: 1 }).then((result) => {
+          expect(result.body).toEqual('Yes, OK')
+        })
+      })
+      it('should ignore empty opts.error', () => {
+        expect.assertions(1)
+        return yfetch({ url: MOCK_URLS.OK, error: [] }).then((result) => {
+          expect(result.body).toEqual('Yes, OK')
+        })
+      })
+      it('should success when opts.error not matched', () => {
+        expect.assertions(1)
+        return yfetch({ url: MOCK_URLS.OK, error: [404, 500] }).then((result) => {
+          expect(result.body).toEqual('Yes, OK')
+        })
+      })
       it('should threat code 200 as error', () => yfetch({ url: MOCK_URLS.OK, error: [200] }).catch((err) => {
         expect(err.message).toEqual('Response Code 200 means Error (includes: 200)')
       }))
-      it('should threat code 200 as error', yfetch({ url: MOCK_URLS.OK, error: [100, 200, 300] }).catch((err) => {
-        expect(err.message).toEqual('Response Code 200 means Error (includes: 100,200,300)')
-        expect(err.response).toEqual({
-          url: 'http://test/ok',
-          status: 200,
-          statusText: 'OK',
-          ok: true,
-          size: 0,
-          body: 'Yes, OK',
-          headers: {},
-          fetchArgs: ['http://test/ok', {
+      it('should threat code 200 as error', () => {
+        yfetch({ url: MOCK_URLS.OK, error: [100, 200, 300] }).catch((err) => {
+          expect.assertions(2)
+          expect(err.message).toEqual('Response Code 200 means Error (includes: 100,200,300)')
+          expect(err.response).toEqual({
+            url: 'http://test/ok',
+            status: 200,
+            statusText: 'OK',
+            ok: true,
+            size: 0,
+            body: 'Yes, OK',
             headers: {},
-            error: [100, 200, 300]
-          }]
+            fetchArgs: ['http://test/ok', {
+              headers: {},
+              error: [100, 200, 300]
+            }]
+          })
         })
-      }))
+      })
     })
   })
 })
